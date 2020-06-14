3636625ca5efb84369b7b48a0879b6c068d2476a
Wahab Shah <Wahab.shah23@gmail.com>
2020-06-14 23:55:58 +0500

    commented out unused code

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Room from './Room';
-import Temp from './Temp';
+// import Temp from './Temp';
 
 function App() {
   return (
--- src/Temp.js
+++ src/Temp.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Temp.css'
 
 function Temp() {
